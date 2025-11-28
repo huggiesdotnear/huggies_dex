16d40a7f353b0da0ef0def76c27c5da0058dfb18
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-11-28 09:59:11 -0600

    re   target: document.getElementById('root')!,

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import App from './App.svelte';
 import './css/main.css';
 
 const app = mount(App, {
-  target: document.body,
+  // target: document.body,
+  // or use my index.html template
+  target: document.getElementById('root')!,
 });
 
 export default app;
