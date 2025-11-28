75d6847fa156fd9395152030880e19e513dd6070
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-11-28 09:47:44 -0600

    PWA

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,7 +5,7 @@ import { pluginSvelte } from '@rsbuild/plugin-svelte';
 export default defineConfig({
   plugins: [pluginSvelte()],
   html: {
-      title: 'HUGGIES',
+    template: './public/index.html',
     },
 
 });
