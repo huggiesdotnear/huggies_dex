6a2dcc74aab2045a09a4d41d633cce9b3c2e8f8c
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-11-28 20:23:30 -0600

    ab

--- src/svelte/page_about.svelte
+++ src/svelte/page_about.svelte
@@ -5,10 +5,12 @@
     <ABOUT_ABOUT />
     <ABOUT_FEATURES />
     <p>USE AT YOUR OWN RISK.</p>
+    <ABOUT_LINKS />
 </section>
 <!--  -->
 <!-- =========================================== -->
 <script lang="ts">
     import ABOUT_ABOUT from "./components/about_about.svelte";
     import ABOUT_FEATURES from "./components/about_features.svelte";
+    import ABOUT_LINKS from "./components/about_links.svelte";
 </script>
