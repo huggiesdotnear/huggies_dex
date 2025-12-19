460097a6ff5565c2817e0a4b1bdd85a6853a431d
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-12-19 17:28:55 -0600

    Update rhea_const.ts

--- src/ts/rhea/rhea_const.ts
+++ src/ts/rhea/rhea_const.ts
@@ -1,2 +1,9 @@
 import { ref_mainnet_contractId_const, ref_testnet_contractId_const } from "@sleet-js/ref-exchange-methods-const";
+import { getStoredNetworkId } from "../network";
 //
+export function ref_contractId_for_network() {
+  const networkId = getStoredNetworkId();
+  const ref_contractId = networkId === "testnet" ? ref_mainnet_contractId_const : ref_testnet_contractId_const;
+  console.log("ref contractId", ref_contractId);
+  return ref_contractId;
+}
