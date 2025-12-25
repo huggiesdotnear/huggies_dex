8c0c1587dcadfebc2f6682c136a99c034cdfa8ec
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-12-25 08:34:28 -0600

    Update test.ts

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -1,15 +1,19 @@
 // this file is to test that the variables are right i wrote for fun.
-import { NETWORK_STORAGE_KEY } from './fastintear/createNearClient'
-import { ref_contractId_for_network } from './ref/ref_const'
-import { ref_get_number_of_pools_function, ref_get_pool_function } from './ref/ref_functions'
+import { nearClient, NETWORK_STORAGE_KEY } from "./fastintear/createNearClient";
+import { ref_contractId_for_network } from "./ref/ref_const";
+import {
+  ref_get_number_of_pools_function,
+  ref_get_pool_function,
+} from "./ref/ref_functions";
 // ===================================
-console.log("============================")
-console.log("THIS IS TO TEST THE RIGHT STUFF IS BEING USED FOR NETWORK")
-console.log(localStorage.getItem(NETWORK_STORAGE_KEY))
-ref_contractId_for_network()
-console.log(await ref_get_number_of_pools_function())
+console.log("============================");
+console.log("THIS IS TO TEST THE RIGHT STUFF IS BEING USED FOR NETWORK");
+console.log(localStorage.getItem(NETWORK_STORAGE_KEY));
+ref_contractId_for_network();
+console.log(await ref_get_number_of_pools_function());
 // console.log(await ref_get_pool_function(5767))
 // ==============================
+console.log("============================");
 async function exampleUsage_of_get_pool() {
   try {
     const pool = await ref_get_pool_function(5767);
@@ -24,6 +28,12 @@ async function exampleUsage_of_get_pool() {
     console.error("Unexpected error in exampleUsage:", error);
   }
 }
-console.log(exampleUsage_of_get_pool())
+console.log(exampleUsage_of_get_pool());
+console.log("============================");
+console.log("============================");
 // ==============================
-console.log("============================")
+// Get all transaction history
+const txHistory = nearClient().localTxHistory();
+console.log(txHistory); // Contains all transaction statuses
+console.log("============================");
+console.log("============================");
