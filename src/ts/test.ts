5f8311198ff3d9d67a4edc0bafe44383da74d8c1
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-12-25 11:28:51 -0600

    Update test.ts

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -1,19 +1,19 @@
 // this file is to test that the variables are right i wrote for fun.
-import { nearClient, NETWORK_STORAGE_KEY } from "./fastintear/createNearClient";
+import { NETWORK_STORAGE_KEY } from "./fastintear/createNearClient";
 import { ref_contractId_for_network } from "./ref/ref_const";
 import {
   ref_get_number_of_pools_function,
   ref_get_pool_function,
 } from "./ref/ref_functions";
 // ===================================
-console.log("============================");
+console.log("==========================================================");
 console.log("THIS IS TO TEST THE RIGHT STUFF IS BEING USED FOR NETWORK");
 console.log(localStorage.getItem(NETWORK_STORAGE_KEY));
 ref_contractId_for_network();
 console.log(await ref_get_number_of_pools_function());
 // console.log(await ref_get_pool_function(5767))
 // ==============================
-console.log("============================");
+console.log("==========================================================");
 async function exampleUsage_of_get_pool() {
   try {
     const pool = await ref_get_pool_function(5767);
@@ -25,15 +25,14 @@ async function exampleUsage_of_get_pool() {
 
     console.log("Pool data:", pool);
   } catch (error) {
-    console.error("Unexpected error in exampleUsage:", error);
+    console.error("Expected or Unexpected error in exampleUsage_of_get_pool!!!");
   }
 }
 console.log(exampleUsage_of_get_pool());
-console.log("============================");
-console.log("============================");
+console.log("==========================================================");
 // ==============================
 // Get all transaction history
-const txHistory = nearClient().localTxHistory();
-console.log(txHistory); // Contains all transaction statuses
-console.log("============================");
-console.log("============================");
+// const txHistory = nearClient().localTxHistory();
+// console.log(txHistory);
+console.log("==========================================================");
+
