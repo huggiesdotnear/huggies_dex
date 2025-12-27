f7a1941caed421e9ef577de87e5ab66851da361e
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-12-27 07:42:02 -0600

    Update profile_ft_transfer_to_rhea_component.svelte

--- src/svelte/components/profile_ft_transfer_to_rhea_component.svelte
+++ src/svelte/components/profile_ft_transfer_to_rhea_component.svelte
@@ -1,4 +1,13 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import {
+    ft_balance_of_function,
+    ft_transfer_call_function,
+  } from "../../ts/ft/ft_functions";
+  import { ref_contractId_for_network } from "../../ts/ref/ref_const";
+  import { get_nearClient_accountId } from "../../ts/fastintear/accountId";
+  // ================================================================
+  
+</script>
 
 <!--  -->
 <!--  -->
