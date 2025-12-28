eb22dd9fb51409271863901d17535b5b5feba3b1
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-12-27 20:04:13 -0600

    Update index.ts

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,7 +1,7 @@
 import { util } from "near-sdk-as";
 import { html } from "./html";
 import { js } from "./sw";
-import { json } from "./manifest";
+import { jsonText } from "./manifest"; // use raw manifest text
 import { svg } from "./svg";
 // ===========================================================
 @nearBindgen
@@ -20,10 +20,31 @@ class Web4Response {
   bodyUrl?: string;
   preloadUrls: string[] = [];
 }
-// =================================
+// ===========================================================
 function htmlResponse(text: string): Web4Response {
   return {
     contentType: "text/html; charset=UTF-8",
+    body: util.stringToBytes(text),
+    status: 200,
+  } as Web4Response;
+}
+function jsonResponse(text: string): Web4Response {
+  return {
+    contentType: "application/json; charset=UTF-8",
+    body: util.stringToBytes(text),
+    status: 200,
+  } as Web4Response;
+}
+function svgResponse(text: string): Web4Response {
+  return {
+    contentType: "image/svg+xml; charset=UTF-8",
+    body: util.stringToBytes(text),
+    status: 200,
+  } as Web4Response;
+}
+function jsResponse(text: string): Web4Response {
+  return {
+    contentType: "application/javascript; charset=UTF-8",
     body: util.stringToBytes(text),
     status: 200,
   } as Web4Response;
@@ -34,8 +55,19 @@ function status(status: u32): Web4Response {
 }
 // ===========================================================
 export function web4_get(request: Web4Request): Web4Response {
-  if (request.path == "/") {
+  const path = request.path ? request.path : "/";
+
+  if (path == "/") {
     return htmlResponse(decodeURI(html));
+  }
+  if (path == "/manifest.json") {
+    return jsonResponse(jsonText);
+  }
+  if (path == "/shit_icon_blue.svg") {
+    return svgResponse(svg);
+  }
+  if (path == "/sw.js") {
+    return jsResponse(js);
   }
 
   return status(404);
