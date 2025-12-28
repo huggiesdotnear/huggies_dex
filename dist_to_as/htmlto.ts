6908005cd3120be46bb56af865d5250f56246f13
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-12-28 08:38:47 -0600

    rm as

--- dist_to_as/htmlto.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-const inputPath = "./dist/index.html";
-const outputPath = "./assembly/html.ts";
-
-const file = fs.readFileSync(inputPath, { encoding: "utf8" });
-fs.writeFileSync(outputPath, `export const html = \`${encodeURI(file)}\`;\n`);
-console.log(`Wrote ${outputPath}`);
