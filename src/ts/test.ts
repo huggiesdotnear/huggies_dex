7fd86e771afa3f48e1c661e9b98451b5cf44e0ab
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-01-05 19:46:10 -0600

    Update test.ts

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -1,10 +1,12 @@
 // this file is to test that the variables are right i wrote for fun.
 import { NETWORK_STORAGE_KEY } from "./fastintear/createNearClient";
 import { ref_contractId_for_network } from "./ref/ref_const";
+import { get_nearClient_accountId } from "./fastintear/accountId";
 import {
   ref_get_number_of_pools_function,
   ref_get_deposits_function,
   ref_get_pool_function,
+  ref_storage_balance_of_function,
 } from "./ref/ref_functions";
 // ===================================
 console.log("==========================================================");
@@ -40,4 +42,9 @@ console.log("==========================================================");
 // Get all transaction history
 // const txHistory = nearClient().localTxHistory();
 // console.log(txHistory);
+console.log("==========================================================");
+const accountId = get_nearClient_accountId()
+console.log(accountId)
+const storage_balance = await ref_storage_balance_of_function(accountId)
+console.log("STORAGE BALANCE:", storage_balance.available, storage_balance.total)
 console.log("==========================================================");
